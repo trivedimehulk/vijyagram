9961c9e01e163069680cf9bc5d348ea1755fd769
trivedimehulk <trivedimehulk@gmail.com>
2025-04-11 20:13:57 -0400

    changes

--- fbase-config.js
+++ fbase-config.js
@@ -9,5 +9,5 @@ const firebaseConfig = {
   };
   
   firebase.initializeApp(firebaseConfig);
-  const storage = firebase.storage();
+  //const storage = firebase.storage();
   const db = firebase.firestore();
